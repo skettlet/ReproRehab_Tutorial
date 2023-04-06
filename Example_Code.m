f29e883355d970e02704628b8cdd7a5216e1964e
skettlet <kettlety@usc.edu>
2023-04-06 10:32:59 -0700

    Changed to example code from ChatGPT

--- Example_Code.m
+++ Example_Code.m
@@ -1,1 +1,18 @@
-Var1 = 6; 
+% This is a simple MATLAB code to calculate the average of two numbers and plot them
+
+% Initialize the variables
+num1 = 5;
+num2 = 7;
+
+% Calculate the average of the two numbers
+average = (num1 + num2) / 2;
+
+% Display the result
+disp(['The average of ' num2str(num1) ' and ' num2str(num2) ' is ' num2str(average)]);
+
+% Plot the two numbers
+figure;
+bar([num1, num2]);
+title('Numbers Plot');
+xlabel('Numbers');
+ylabel('Values');
